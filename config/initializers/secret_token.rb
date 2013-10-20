c1f009104551c9c6f02e5eea59f492e1ef3c0cf5
Jeff Donovan <happyguitar@gmail.com>
2013-10-20 09:03:36 -0500

    Initial commit after scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tickets2::Application.config.secret_token = '3d9434e4ff7a5cd40dbd39647754420a5900b6311a79066243a66d7c9819b744924285b85e17a781cc6b4647c2ddc76bd39ca5ca93513a420bf65f6f1f6f81c6'
