b8c4cabb80947c33b962f9aa399adad20e3116b0
Jeff Donovan <happyguitar@gmail.com>
2013-10-20 11:16:29 -0500

    Ran rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131020134619) do
+ActiveRecord::Schema.define(:version => 20131020155253) do
 
   create_table "tickets", :force => true do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20131020134619) do
     t.string   "email_address"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+    t.string   "phone"
   end
 
 end
